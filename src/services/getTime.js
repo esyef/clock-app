7b516a7f05cfa4ac60aa6131b2bb292e8895fef7
YefWeb <yeferson9828@gmail.com>
2022-03-18 23:09:39 -0500

    Services remove and API called in TimeState

--- src/services/getTime.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export const getTime = async (zone, city) => {
-	const res = await axios.get(
-		`http://worldtimeapi.org/api/timezone/${zone}/${city}`
-	)
-	const data = await res.data
-	return data
-}
