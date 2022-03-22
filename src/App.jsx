63c925a152df789b9deffe79abb93f3d120ec55a
YefWeb <yeferson9828@gmail.com>
2022-03-21 22:23:27 -0500

    footer and  MD added

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,22 @@ const App = () => {
 					<Expand />
 				</ExpandState>
 			</Layaout>
+			<footer className={styles.footer}>
+				{' '}
+				Coder{' '}
+				<a href='https://www.yefweb.com' target='_blank' rel='noreferrer'>
+					YefWeb
+				</a>
+				{' |  '}
+				Challenge By{'  '}
+				<a
+					href='https://www.frontendmentor.io/home'
+					target='_blank'
+					rel='noreferrer'
+				>
+					Frontend Mentor
+				</a>
+			</footer>
 		</TimeState>
 	)
 }
