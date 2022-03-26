0a1a5711dd556d989168427ffc43d36f5b7222bd
YefWeb <yeferson9828@gmail.com>
2022-03-25 20:31:37 -0500

    footer fixed

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
 	return (
-		<footer className='text-center text-xs bg-slate-500/10 p-2 absolute bottom-0 left-0 right-0'>
+		<footer className='text-center text-xs bg-slate-500/10 p-2 '>
 			{' '}
 			Coder{' '}
 			<a href='https://www.yefweb.com' target='_blank' rel='noreferrer'>
